dd07f65106c2fbd06cdf1a1dcfc68b6a9053c212
Vladislav.main <vladislav.sharshov@gmail.com>
2023-10-10 14:04:38 +0200

    v.1.0

---
+++ zip-loop.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+#!/usr/bin/python
+
+from pathlib import Path
+import os,sys
+
+print(f'CREATE p_name№s_name.zip FROM ALL p_name№.s_name* FILES IN USING DIRECTORY')
+
+f_path = input("Enter path to the directory containing files for zipping (press ENTER for use current directory): ")
+if f_path =='':
+    u_path = '.'
+else:
+    u_path = str(f_path)
+os.chdir(u_path)    
+print(f'Using directory: {os.getcwd()}')
+
+p_name = input("Enter name (before №): ")
+s_name = input("Enter second static part of name (after №) OPTIONAL (press ENTER if you have not it): ")
+i = int(input("Enter first number (№): "))
+n = int(input("Enter last number (№): "))
+
+
+while i<=n:
+    print(i)
+    os.system(f'zip {p_name}{i}{s_name}.zip {p_name}{i}.{s_name}*')
+    i+=1
+
+print(f'Ziping successfully completed! in directory: {os.getcwd()}')
+
